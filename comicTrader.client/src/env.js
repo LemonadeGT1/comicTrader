0e3a0be1629ace4776d4c70f53fea8cf905a1589
LemonadeGT1 <michael.p.patience@gmail.com>
2023-06-01 17:47:24 -0600

    init

---
+++ comicTrader.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-v3bz7ala46wwesk6.us.auth0.com'
+export const clientId = 'VI2WvrMVvaOvTIhEcAk6Cu913MBB3FIh'
+export const audience = 'http://BCWSandbox.io'
