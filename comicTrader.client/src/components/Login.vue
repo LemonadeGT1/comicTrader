28d37baa1780fbf2b093119d2beedabb3ebfb80f
LemonadeGT1 <michael.p.patience@gmail.com>
2023-06-02 20:51:58 -0600

    fixed profile pic

--- comicTrader.client/src/components/Login.vue
+++ comicTrader.client/src/components/Login.vue
@@ -8,7 +8,7 @@
       <div class="dropdown my-2 my-lg-0">
         <div type="button" class="border-0 selectable no-select" data-bs-toggle="dropdown" aria-expanded="false">
           <div v-if="account.picture || user.picture">
-            <img :src="account.picture || user.picture" alt="account photo" height="40" class="rounded" />
+            <img :src="account.picture || user.picture" alt="account photo" height="40" class="profilePic" />
           </div>
         </div>
         <div class="dropdown-menu dropdown-menu-lg-end dropdown-menu-start p-0" aria-labelledby="authDropdown">
@@ -49,4 +49,8 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.profilePic {
+  border-radius: 50px;
+}
+</style>
