2ca806ea5f72d9c23eee85ef7caf47f5add9caf9
LemonadeGT1 <michael.p.patience@gmail.com>
2023-06-27 14:07:32 -0600

    additional details added to comics

--- comicTrader.client/src/components/ViewComicDetails.vue
+++ comicTrader.client/src/components/ViewComicDetails.vue
@@ -4,8 +4,18 @@
       <img :src="activeComic.thumbnail?.path + '.' + activeComic.thumbnail?.extension" class="img-fluid comicImg2 rounded"
         :title="activeComic.title" />
     </div>
-    <p class="pt-3" v-html="activeComic.description"></p>
-    <p v-if="activeComic.pageCount > 0" class="pageCount mb-0">Page Count: <span v-text="activeComic.pageCount"></span>
+    <p v-if="activeComic.description" class="pt-3 mb-0" v-html="activeComic.description"></p>
+    <p class="pt-3 mb-0" v-if="parseInt(activeComic.issueNumber, 10) > 0">Issue Number: {{ activeComic.issueNumber }}</p>
+    <p v-if="activeComic.pageCount > 0" class="pageCount mb-0 pt-3">Page Count: <span
+        v-text="activeComic.pageCount"></span>
+    </p>
+    <p class="pt-3">Creators:
+    <table>
+      <tr v-for="c in activeComic.creators.items">
+        <td class="ps-3 text-end">{{ c.role.charAt(0).toUpperCase() + c.role.slice(1) }}</td>
+        <td class="ps-3 text-end">{{ c.name }}</td>
+      </tr>
+    </table>
     </p>
   </span>
 </template>
